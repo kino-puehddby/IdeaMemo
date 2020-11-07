f2ff53ac455de58f31cb8352a1f885af6c243d22
HisayaSugita( kino ) <pswty180@gmail.com>
2020-11-07 17:03:25 +0900

    不要なファイルを削除

--- update.sh
+++
@@ -1,6 +0,0 @@
-# setup Carthage variables ( for Xcode 12.x )
-# update Carthage Libraries ( from Cartfile )
-sh setup-carthage.sh update --platform iOS --no-use-binaries
-
-# update CocoaPods Libraries ( from Podfile )
-pod update
