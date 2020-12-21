d3483db5b7a0f8b26620d6fba49b8061e3efcf84
HisayaSugita( kino ) <pswty180@gmail.com>
2020-12-22 01:13:12 +0900

    ログイン画面の追加

---
+++ IdeaMemo/View/AuthenticationView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationView.swift
+//  IdeaMemo
+//
+//  Created by Hisaya Sugita on 2020/12/19.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text("ログイン/新規登録画面")
+    }
+}
+
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationView()
+    }
+}
