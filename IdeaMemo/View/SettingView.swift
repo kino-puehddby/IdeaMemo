0f1d0def53bae2c852f38756ac4efc769c8e2073
HisayaSugita( kino ) <pswty180@gmail.com>
2021-01-03 17:20:50 +0900

    SettingViewを追加

---
+++ IdeaMemo/View/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  IdeaMemo
+//
+//  Created by Hisaya Sugita on 2021/01/01.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
