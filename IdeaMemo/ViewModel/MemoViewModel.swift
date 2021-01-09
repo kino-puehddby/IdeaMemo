7cc2be253fca5780a79871ec1255091b7bd784e9
HisayaSugita( kino ) <pswty180@gmail.com>
2021-01-09 20:32:13 +0900

    メモ画面に前回保存された内容が反映されない問題を解消

--- IdeaMemo/ViewModel/MemoViewModel.swift
+++ IdeaMemo/ViewModel/MemoViewModel.swift
@@ -31,24 +31,22 @@ final class MemoViewModel: ObservableObject, Identifiable {
                 $title.removeDuplicates(),
                 $content.removeDuplicates()
             )
+            .dropFirst()
             .receive(on: RunLoop.main)
             .sink { [weak self] title, content in
                 guard let self = self else { return }
                 self.title = title
                 self.content = content
-                guard let memo = memo else { return }
+                guard let memo = self.cached.value else { return }
                 self.cached.value = Memo(id: memo.id, title: title, content: content)
             }
             .store(in: &self.cancellables)
 
-        Publishers
-            .Zip(
-                commitEvent,
-                disappearEvent
-            )
+        commitEvent
             .combineLatest(cached)
             .map { $1 }
             .filterNil()
+            .removeDuplicates()
             .flatMap { memo -> AnyPublisher<Memo, Error> in
                 return CloudMemoRecord.update(memo: memo)
             }
@@ -63,6 +61,7 @@ final class MemoViewModel: ObservableObject, Identifiable {
             .store(in: &self.cancellables)
         
         ApplicationStore.shared.memoState.map { $0.error }
+            .dropFirst()
             .sink { [weak self] error in
                 guard let self = self else { return }
                 guard let error = error else {
@@ -76,17 +75,21 @@ final class MemoViewModel: ObservableObject, Identifiable {
     
     private(set) lazy var onAppear: () -> Void = { [unowned self] in
         Just(self.cached.value)
-            .filter { $0 == nil }
             .sink { [unowned self] memo in
-                // 新規作成されたメモの場合は、一旦 iCloud 上にデータを保存する
-                CloudMemoRecord.create(title: "", content: "") { result in
-                    switch result {
-                    case .success(let memo):
-                        self.cached.value = memo
-                    case .failure(let error):
-                        ApplicationStore.shared.dispatch(MemoState.Action.error(error))
+                guard let memo = memo else {
+                    // 新規作成されたメモの場合は、一旦 iCloud 上にデータを保存する
+                    CloudMemoRecord.create(title: "", content: "") { result in
+                        switch result {
+                        case .success(let memo):
+                            self.cached.value = memo
+                        case .failure(let error):
+                            ApplicationStore.shared.dispatch(MemoState.Action.error(error))
+                        }
                     }
+                    return
                 }
+                self.title = memo.title
+                self.content = memo.content
             }
             .store(in: &self.cancellables)
     }
