e18d884d5483f9f55d7d93c8f21b9851ee919e86
HisayaSugita( kino ) <pswty180@gmail.com>
2021-01-09 19:01:47 +0900

    ホーム画面のMemoRowで動的に状態変化が反映されるようにする

--- IdeaMemo/View/Home/MemoRow.swift
+++ IdeaMemo/View/Home/MemoRow.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct MemoRow: View {
-    let memo: Memo
+    @ObservedObject var viewModel: MemoRowViewModel
+    
+    init(memo: Memo) {
+        viewModel = MemoRowViewModel(memo: memo)
+    }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 5) {
-            Text(memo.title)
+            Text(viewModel.memo.title)
                 .font(.headline)
                 .foregroundColor(Color(Asset.Colors.primaryContentColor.color))
-            Text(memo.content)
+            Text(viewModel.memo.content)
                 .font(.subheadline)
                 .foregroundColor(Color(Asset.Colors.secondaryContentColor.color))
         }
